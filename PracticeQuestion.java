28cf3ec6e5ad0f95060719f540a3aecac2b788f8
keshav_rajput18 <109911440+keshav-rajput18@users.noreply.github.com>
2022-12-10 19:08:38 +0530

    Add files via upload

---
+++ PracticeQuestion.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class PracticeQuestion {
+    //to calculate the percentage of student
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        /*Question 1
+        System.out.println("Enter the 1st num");
+        int a= sc.nextInt();
+        System.out.println("Enter the 2nd num");
+        int b= sc.nextInt();
+        System.out.println("Enter the 3rd num");
+        int c= sc.nextInt();
+        int sum= a+b+c;
+        System.out.println(sum);*/
+
+        //Question 2
+        // calculate cgpa
+        /*float a=85;
+        float b=68;
+        float c=54;
+        float cgpa=(a+b+c)/30;
+        System.out.println(cgpa);*/
+
+        //Question 3
+        /*System.out.println("Enter your Beautiful Name");
+        String name= sc.next();
+        System.out.println("Hello "+name+",have a good day");*/
+
+        //Question 4
+        //convertor kilometer to miles
+        /*System.out.println("Enter kilometer: ");
+        float a= sc.nextInt();
+        double miles= (a/1.609);
+        System.out.println("Miles: "+miles);*/
+
+        //Question 5
+        //Check integer or not
+        /*System.out.println("Enter the num");
+        System.out.println(sc.hasNextInt());
+
+    }
+}
